4e8461075f2f6b55c5958cac07c350c4b8051d5a
severoleonardo <2020.leonardosevero@gmail.com>
2022-05-18 20:21:36 -0300

    React Responsive Portfolio Website project-de-portefeuille

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from "./Component/Head/Header"
+import "./App.css"
+import Home from "./Component/Hero/Home"
+import Features from "./Component/Features/Features"
+import Portfolio from "./Component/Portfolio/Portfolio"
+
+const App = () => {
+  return (  
+    <>
+      <Header />
+      <Home />
+      <Features />
+      <Portfolio />
+    </>
+  )
+}
+
+export default App
---
+++ src/Component/Portfolio/Portfolio.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Portfolio.css"
+
+
+const Portfolio = () => {
+  return (
+    <>
+        Portfolio
+        {/* <section className="portfolio top" id="portfolio"> */}
+    </>
+  )
+}
+
+export default Portfolio
